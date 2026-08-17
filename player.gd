8825db2fec1f62b29b68ce95858c6caeb396c971
CptTiki <CptTiki@gmail.com>
2026-08-17 19:57:24 -0400

    Protype update

--- player.gd
+++ player.gd
@@ -5,7 +5,7 @@ const SPEED = 4.25
 const JUMP_VELOCITY = 4.5
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	## Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
